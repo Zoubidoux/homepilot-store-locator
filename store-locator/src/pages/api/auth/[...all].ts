822f6cd259c98f8352b4062527f553f79a09e4bd
Zoubidoux <francois.cambillard@gmail.com>
2025-12-23 15:12:30 +0100

    Delete store-locator directory

--- store-locator/src/pages/api/auth/[...all].ts
+++
@@ -1,6 +0,0 @@
-import { createAuth } from "../../../lib/auth";
-import type { APIContext } from "astro";
-
-export const ALL = (context: APIContext) => {
-  return createAuth(context.locals).handler(context.request);
-};
--- store-locator/webpack.webflow.cjs
+++
@@ -1,4 +0,0 @@
-// webpack.config.js (or wherever you set externals)
-module.exports = {
-  mode: "development",
-};
